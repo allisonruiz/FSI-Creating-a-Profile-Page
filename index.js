d3045c2348d9dc4d50253e7a1c5460662bb09a6c
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-02-02 20:15:21 -0800

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let content = document.querySelector('.js-generated.content')
 let content = document.querySelector('.js-generated.content')
 let header = document.createElement('h1')              
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+content.append(header)
